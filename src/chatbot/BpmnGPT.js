ee04fbff8767a80d99674f84a7ace32430270444
Sebastian Stamm <sebastian.stamm@camunda.com>
2024-06-24 10:51:39 +0200

    remove OpenAI
    - add bedrock

--- src/chatbot/BpmnGPT.js
+++ src/chatbot/BpmnGPT.js
@@ -1,15 +1,15 @@
-import Mustache from 'mustache';
+import Mustache from "mustache";
 
-Mustache.escape = text => text;
+Mustache.escape = (text) => text;
 
-import OpenAI from 'openai';
+// import OpenAI from 'openai';
 
-const openAIApiKey = process.env.OPENAI_API_KEY;
+// const openAIApiKey = process.env.OPENAI_API_KEY;
 
-const openai = new OpenAI({
-  apiKey: openAIApiKey,
-  dangerouslyAllowBrowser: true
-});
+// const openai = new OpenAI({
+//   apiKey: openAIApiKey,
+//   dangerouslyAllowBrowser: true
+// });
 
 const baseInstructions = `Events should be labeled using object + past participle.
 Start events should always be labeled with an indication of the trigger of the process.
@@ -23,135 +23,138 @@ End events must have one incoming sequence flow.
 All other activities must have at least one of each.`;
 
 const validJson = {
-  'id': 'Process_1',
-  'description': 'Hiring process',
-  'elements': [
+  id: "Process_1",
+  description: "Hiring process",
+  elements: [
     {
-      'id': 'StartEvent_1',
-      'name': 'Application Received',
-      'type': 'bpmn:StartEvent',
-      'parent': 'Process_1'
+      id: "StartEvent_1",
+      name: "Application Received",
+      type: "bpmn:StartEvent",
+      parent: "Process_1",
     },
     {
-      'id': 'Task_1',
-      'name': 'Screen Application',
-      'type': 'bpmn:Task',
-      'parent': 'Process_1'
+      id: "Task_1",
+      name: "Screen Application",
+      type: "bpmn:Task",
+      parent: "Process_1",
     },
     {
-      'id': 'ExclusiveGateway_1',
-      'name': 'Is applicant qualified?',
-      'type': 'bpmn:ExclusiveGateway',
-      'parent': 'Process_1'
+      id: "ExclusiveGateway_1",
+      name: "Is applicant qualified?",
+      type: "bpmn:ExclusiveGateway",
+      parent: "Process_1",
     },
     {
-      'id': 'Task_2',
-      'name': 'Invite for Interview',
-      'type': 'bpmn:Task',
-      'parent': 'Process_1'
+      id: "Task_2",
+      name: "Invite for Interview",
+      type: "bpmn:Task",
+      parent: "Process_1",
     },
     {
-      'id': 'IntermediateCatchEvent_1',
-      'name': 'Invitation Accepted',
-      'type': 'bpmn:IntermediateCatchEvent',
-      'eventDefinitionType': 'bpmn:MessageEventDefinition',
-      'parent': 'Process_1'
+      id: "IntermediateCatchEvent_1",
+      name: "Invitation Accepted",
+      type: "bpmn:IntermediateCatchEvent",
+      eventDefinitionType: "bpmn:MessageEventDefinition",
+      parent: "Process_1",
     },
     {
-      'id': 'Task_3',
-      'name': 'Conduct Interview',
-      'type': 'bpmn:Task',
-      'parent': 'Process_1'
+      id: "Task_3",
+      name: "Conduct Interview",
+      type: "bpmn:Task",
+      parent: "Process_1",
     },
     {
-      'id': 'EndEvent_1',
-      'name': 'Interview Conducted',
-      'type': 'bpmn:EndEvent',
-      'parent': 'Process_1'
+      id: "EndEvent_1",
+      name: "Interview Conducted",
+      type: "bpmn:EndEvent",
+      parent: "Process_1",
     },
     {
-      'id': 'Task_4',
-      'name': 'Send Rejection Email',
-      'type': 'bpmn:Task',
-      'parent': 'Process_1'
+      id: "Task_4",
+      name: "Send Rejection Email",
+      type: "bpmn:Task",
+      parent: "Process_1",
     },
     {
-      'id': 'EndEvent_2',
-      'name': 'Applicant Rejected',
-      'type': 'bpmn:EndEvent',
-      'parent': 'Process_1'
+      id: "EndEvent_2",
+      name: "Applicant Rejected",
+      type: "bpmn:EndEvent",
+      parent: "Process_1",
     },
     {
-      'id': 'SequenceFlow_1',
-      'name': '',
-      'type': 'bpmn:SequenceFlow',
-      'source': 'StartEvent_1',
-      'target': 'Task_1',
-      'parent': 'Process_1'
+      id: "SequenceFlow_1",
+      name: "",
+      type: "bpmn:SequenceFlow",
+      source: "StartEvent_1",
+      target: "Task_1",
+      parent: "Process_1",
     },
     {
-      'id': 'SequenceFlow_2',
-      'name': '',
-      'type': 'bpmn:SequenceFlow',
-      'source': 'Task_1',
-      'target': 'ExclusiveGateway_1',
-      'parent': 'Process_1'
+      id: "SequenceFlow_2",
+      name: "",
+      type: "bpmn:SequenceFlow",
+      source: "Task_1",
+      target: "ExclusiveGateway_1",
+      parent: "Process_1",
     },
     {
-      'id': 'SequenceFlow_3',
-      'name': 'Yes',
-      'type': 'bpmn:SequenceFlow',
-      'source': 'ExclusiveGateway_1',
-      'target': 'Task_2',
-      'parent': 'Process_1'
+      id: "SequenceFlow_3",
+      name: "Yes",
+      type: "bpmn:SequenceFlow",
+      source: "ExclusiveGateway_1",
+      target: "Task_2",
+      parent: "Process_1",
     },
     {
-      'id': 'SequenceFlow_4',
-      'name': '',
-      'type': 'bpmn:SequenceFlow',
-      'source': 'Task_2',
-      'target': 'IntermediateCatchEvent_1',
-      'parent': 'Process_1'
+      id: "SequenceFlow_4",
+      name: "",
+      type: "bpmn:SequenceFlow",
+      source: "Task_2",
+      target: "IntermediateCatchEvent_1",
+      parent: "Process_1",
     },
     {
-      'id': 'SequenceFlow_5',
-      'name': '',
-      'type': 'bpmn:SequenceFlow',
-      'source': 'IntermediateCatchEvent_1',
-      'target': 'Task_3',
-      'parent': 'Process_1'
+      id: "SequenceFlow_5",
+      name: "",
+      type: "bpmn:SequenceFlow",
+      source: "IntermediateCatchEvent_1",
+      target: "Task_3",
+      parent: "Process_1",
     },
     {
-      'id': 'SequenceFlow_6',
-      'name': '',
-      'type': 'bpmn:SequenceFlow',
-      'source': 'Task_3',
-      'target': 'EndEvent_1',
-      'parent': 'Process_1'
+      id: "SequenceFlow_6",
+      name: "",
+      type: "bpmn:SequenceFlow",
+      source: "Task_3",
+      target: "EndEvent_1",
+      parent: "Process_1",
     },
     {
-      'id': 'SequenceFlow_7',
-      'name': 'No',
-      'type': 'bpmn:SequenceFlow',
-      'source': 'ExclusiveGateway_1',
-      'target': 'Task_4',
-      'parent': 'Process_1'
+      id: "SequenceFlow_7",
+      name: "No",
+      type: "bpmn:SequenceFlow",
+      source: "ExclusiveGateway_1",
+      target: "Task_4",
+      parent: "Process_1",
     },
     {
-      'id': 'SequenceFlow_8',
-      'name': '',
-      'type': 'bpmn:SequenceFlow',
-      'source': 'Task_4',
-      'target': 'EndEvent_2',
-      'parent': 'Process_1'
-    }
-  ]
+      id: "SequenceFlow_8",
+      name: "",
+      type: "bpmn:SequenceFlow",
+      source: "Task_4",
+      target: "EndEvent_2",
+      parent: "Process_1",
+    },
+  ],
 };
 
-const formatInstructions = Mustache.render(`'The output must be JSON and JSON only! Here is an example of a valid JSON output:
-{{validJson}}'`, {
-  validJson: JSON.stringify(validJson)
-});
+const formatInstructions = Mustache.render(
+  `'The output must be JSON and JSON only! Here is an example of a valid JSON output:
+{{validJson}}'`,
+  {
+    validJson: JSON.stringify(validJson),
+  }
+);
 
 const createProcessSystemPrompt = `You are a BPMN expert that creates a BPMN process according to a description.
 All BPMN processes you create must be valid, e.g., all elements must be connected.
@@ -174,14 +177,14 @@ export class BpmnGPT {
     const response = await this._getCompletion({
       systemPrompt: Mustache.render(createProcessSystemPrompt, {
         baseInstructions,
-        formatInstructions
+        formatInstructions,
       }),
-      userPrompt: description
+      userPrompt: description,
     });
 
     const end = Date.now();
 
-    console.log('Time elapsed:', (end - start) / 1000, 'seconds');
+    console.log("Time elapsed:", (end - start) / 1000, "seconds");
 
     console.log(response);
 
@@ -194,18 +197,21 @@ export class BpmnGPT {
     const response = await this._getCompletion({
       systemPrompt: Mustache.render(updateProcessSystemPrompt, {
         baseInstructions,
-        formatInstructions
+        formatInstructions,
       }),
-      userPrompt: Mustache.render(`Process: {{process}}
-Requested changes: {{requestedChanges}}`, {
-        process,
-        requestedChanges
-      })
+      userPrompt: Mustache.render(
+        `Process: {{process}}
+Requested changes: {{requestedChanges}}`,
+        {
+          process,
+          requestedChanges,
+        }
+      ),
     });
 
     const end = Date.now();
 
-    console.log('Time elapsed:', (end - start) / 1000, 'seconds');
+    console.log("Time elapsed:", (end - start) / 1000, "seconds");
 
     console.log(response);
 
@@ -213,23 +219,28 @@ Requested changes: {{requestedChanges}}`, {
   }
 
   async _getCompletion({ systemPrompt, userPrompt }) {
-    console.log('systemPrompt', systemPrompt);
-    console.log('userPrompt', userPrompt);
+    console.log("systemPrompt", systemPrompt);
+    console.log("userPrompt", userPrompt);
 
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [
-        {
-          'role': 'system',
-          'content': systemPrompt
-        },
-        {
-          'role': 'user',
-          'content': userPrompt
-        }
-      ],
-      model: 'gpt-4-1106-preview',
-      response_format: { type: 'json_object' }
-    });
+    // const chatCompletion = await openai.chat.completions.create({
+    //   messages: [
+    //     {
+    //       'role': 'system',
+    //       'content': systemPrompt
+    //     },
+    //     {
+    //       'role': 'user',
+    //       'content': userPrompt
+    //     }
+    //   ],
+    //   model: 'gpt-4-1106-preview',
+    //   response_format: { type: 'json_object' }
+    // });
+
+    // TODO: Replace with prediction from Bedrock
+    const chatCompletion = {
+      choices: [{ message: { content: JSON.stringify(validJson) } }],
+    };
 
     const { choices = [] } = chatCompletion;
 
@@ -237,10 +248,10 @@ Requested changes: {{requestedChanges}}`, {
       return null;
     }
 
-    const { message } = choices[ 0 ];
+    const { message } = choices[0];
 
     const { content } = message;
 
-    return content === 'NULL' ? null : JSON.parse(content);
+    return content === "NULL" ? null : JSON.parse(content);
   }
 }
